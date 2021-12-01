9076bdcf6cf6245f2d2a8f39c1505859d2f68481
back hae ran <idg9833@gmail.com>
2021-12-01 11:21:39 +0900

    12월 1일 장고 수업 중

--- mydjango13/mall/models.py
+++ mydjango13/mall/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Shop(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.TextField(blank=True)
     photo = models.ImageField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
