0bd347a82a99dac2b0f1f78dcb5557e6cfd7f451
back hae ran <idg9833@gmail.com>
2021-12-01 18:00:15 +0900

    mall/index 뷰 구현

--- myhomework13/myhomework13/urls.py
+++ myhomework13/myhomework13/urls.py
@@ -18,11 +18,13 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
+from mall13.views import index
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',index)
 ]
 
 urlpatterns += static(settings.MEDIA_URL,
