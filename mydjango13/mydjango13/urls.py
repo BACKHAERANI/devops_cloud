93df15f01da3afd4abbaa59ccd50a0ebc9cd747d
back hae ran <idg9833@gmail.com>
2021-12-01 13:54:25 +0900

    debug 추가

--- mydjango13/mydjango13/urls.py
+++ mydjango13/mydjango13/urls.py
@@ -14,12 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +26,7 @@ urlpatterns = [
 
 urlpatterns+= static(settings.MEDIA_URL,
                       document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls)),]
