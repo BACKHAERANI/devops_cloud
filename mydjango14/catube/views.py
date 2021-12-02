b2288aa8cff0140e05b76bd7f809c17b50d86688
back hae ran <idg9833@gmail.com>
2021-12-02 15:47:26 +0900

    CBV 구현

--- mydjango14/catube/views.py
+++ mydjango14/catube/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 from catube.models import Video
+from django.views.generic import ListView
 
 
-def index(request: HttpRequest) -> HttpResponse:
-    qs = Video.objects.all()
-    return render(
-        request,
-        "catube/index.html",
-        {"video_list": qs},
-    )
+# class based view(CBV)
+index = ListView.as_view(model=Video, template_name="catube/index.html")
+
+# Function based view (FBV)
+# def index(request: HttpRequest) -> HttpResponse:
+#     qs = Video.objects.all()
+#     return render(
+#         request,
+#         "catube/index.html",
+#         {"video_list": qs},
+#     )
 
 
 def video_detail(request: HttpRequest, pk=int) -> HttpResponse:
--- mydjango14/mydjango14/settings.py
+++ mydjango14/mydjango14/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-b4wjq-a&x%wh*q^ar-vn$k!$lx8u=5yndbhiuwr0+w%p6xo!#w
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -41,7 +39,7 @@ INSTALLED_APPS = [
 ]
 
 if DEBUG:
-    INSTALLED_APPS +=['debug_toolbar',]
+    INSTALLED_APPS += ['debug_toolbar', ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -55,8 +53,8 @@ MIDDLEWARE = [
 
 if DEBUG:
     MIDDLEWARE = [
-        'debug_toolbar.middleware.DebugToolbarMiddleware'
-    ] +MIDDLEWARE
+                     'debug_toolbar.middleware.DebugToolbarMiddleware'
+                 ] + MIDDLEWARE
 
 ROOT_URLCONF = 'mydjango14.urls'
 
@@ -78,7 +76,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mydjango14.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -88,7 +85,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -108,7 +104,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -121,7 +116,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
