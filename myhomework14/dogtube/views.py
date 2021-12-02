d2eab6014d0ceea0e47d7eb92827b09f051f0a45
back hae ran <idg9833@gmail.com>
2021-12-02 20:58:05 +0900

    django-imagekit 썸네일처리 및 urls, settings, index(5),layout(3),views(3) 전면 수정

--- myhomework14/dogtube/views.py
+++ myhomework14/dogtube/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 from dogtube.models import Video
 
+# Class Based View (CBV)
+# index = ListView.as_view(model=Video, template_name="catube/index.html")
+
+
+# Function Based View (FBV)
+
 
 def index(request: HttpRequest) -> HttpResponse:
     qs = Video.objects.all()
