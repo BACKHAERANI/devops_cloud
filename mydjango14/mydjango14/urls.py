295a3d5dce3e0a04759929b6d49e73aa091cc1c7
back hae ran <idg9833@gmail.com>
2021-12-02 11:09:57 +0900

    html 생성 및 cdn 적용

--- mydjango14/mydjango14/urls.py
+++ mydjango14/mydjango14/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import  settings
+from django.conf import settings
 from django.conf.urls.static import static
+from catube.views import index
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('catube/',index),]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
@@ -15,3 +16,4 @@ urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns +=[path('__debug__/', include(debug_toolbar.urls)),]
+
