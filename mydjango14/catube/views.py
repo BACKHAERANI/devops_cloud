1e90379906a9323831f68976512f8903be6b37a9
back hae ran <idg9833@gmail.com>
2021-12-02 15:21:00 +0900

    비디오 재생 기능 추가

--- mydjango14/catube/views.py
+++ mydjango14/catube/views.py
@@ -10,3 +10,12 @@ def index(request: HttpRequest) -> HttpResponse:
         "catube/index.html",
         {"video_list": qs},
     )
+
+
+def video_detail(request: HttpRequest, pk=int) -> HttpResponse:
+    video = Video.objects.get(pk=pk)
+    return render(
+        request,
+        "catube/video_detail.html",
+        {"video": video},
+    )
