6835d8f5aad117885a919d1c820089b3488a83b1
back hae ran <idg9833@gmail.com>
2021-12-02 14:55:50 +0900

    catube models, views, admin

--- mydjango14/catube/models.py
+++ mydjango14/catube/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Video(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    video_file = models.FileField()
+    thumbnail = models.ImageField()
+    # TODO = 업로드되는 파일이 비디오파일인지 여부를 검사하는 게 있으면 좋아
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
