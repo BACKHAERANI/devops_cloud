bdf705714d805d7a16a012685c66e1333de2feae
back hae ran <idg9833@gmail.com>
2021-12-03 16:45:08 +0900

    mydjango15 오후 추가사항

--- mydjango15/delicious/models.py
+++ mydjango15/delicious/models.py
@@ -1,14 +1,19 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 
 class Shop(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
+    name = models.CharField(max_length=100, db_index=True)   #디비에서 이름은 샵엔엠이다
     description = models.TextField(blank=True)
     address = models.CharField(max_length=100)
     # TODO: GEODJANGO의 POINTFIELD를 사용
     latitude = models.FloatField(verbose_name="위도")
     longitude = models.FloatField(verbose_name="경도")
     #TODO: 전화번호 값인지 여부를 체킹
-    telephone = models.CharField(max_length=15)
+    telephone = models.CharField(max_length=15,
+                                 validators=[
+                                     RegexValidator(r"\d{3,4}-?/d{4}$",
+                                                    message="전화번호를 입력해주세요.")
+                                 ])
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- mydjango15/delicious/views.py
+++ mydjango15/delicious/views.py
@@ -1,15 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
+from django.views.generic import CreateView
+from delicious.form import ShopForm
 from delicious.models import Shop
 
 
 def shop_list(request: HttpRequest) -> HttpResponse:
     qs = Shop.objects.all()
+    #querystring에 guery 이름의 인자를 확인
+    query = request.GET.get("query","")
+    if query:
+        qs = qs.filter(name__icontains=query)
+
     template_name = "delicious/shop_list.html"
     context_data = {
         "shop_list":qs,
     }
-    return render(request,template_name,context_data)
+    return render(request,template_name,context_data)  #지정인자로 받기
 
 
 def shop_detail(request:HttpRequest, pk: int) -> HttpResponse:
@@ -19,3 +26,32 @@ def shop_detail(request:HttpRequest, pk: int) -> HttpResponse:
         "shop": shop,
     }
     return render(request, template_name, context_data)
+
+
+def shop_new_1(request:HttpRequest)->HttpResponse:
+    if request.method == "GET":                          #파이썬은 대문자 아니면 소문자로 통일하여 post나 get 입력
+        return render(request,"delicious/shop_form_1.html",{})
+    else:
+        name = request.POST["name"]
+        description = request.POST["description"]
+        address = request.POST["address"]
+        latitude = request.POST["latitude"]
+        longitude = request.POST["longitude"]
+        telephone = request.POST["telephone"]
+        # 유효성검사 필요
+        Shop.objects.create(
+            name=name,
+            description=description,
+            address=address,
+            latitude=latitude,
+            longitude=longitude,
+            telephone=telephone,
+        )
+        return redirect("/delicious/")
+
+
+shop_new = CreateView.as_view(
+    model=Shop,
+    form_class=ShopForm,
+    success_url="/delicious/",
+)
