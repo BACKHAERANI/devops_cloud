212fe95fb4b5ab165b0bbbefa4e0213650f67620
back hae ran <idg9833@gmail.com>
2021-12-03 14:23:03 +0900

    delicious models,views,admin 구현

--- mydjango15/delicious/views.py
+++ mydjango15/delicious/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from delicious.models import Shop
 
-# Create your views here.
+
+def shop_list(request: HttpRequest) -> HttpResponse:
+    qs = Shop.objects.all()
+    template_name = "delicious/shop_list.html"
+    context_data = {
+        "shop_list":qs,
+    }
+    return render(request,template_name,context_data)
+
+
+def shop_detail(request:HttpRequest, pk: int) -> HttpResponse:
+    shop = Shop.objects.get(pk= pk)
+    template_name = "delicious/shop_detail.html"
+    context_data={
+        "shop": shop,
+    }
+    return render(request, template_name, context_data)
