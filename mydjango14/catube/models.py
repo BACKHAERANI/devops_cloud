5e6dd9ad36ba4da8041ae05bccf64a3be36f42c5
back hae ran <idg9833@gmail.com>
2021-12-03 16:55:02 +0900

    mydjango14 누락업로드

--- mydjango14/catube/models.py
+++ mydjango14/catube/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFill
 
 
 class Video(models.Model):
@@ -6,6 +8,12 @@ class Video(models.Model):
     description = models.TextField()
     video_file = models.FileField()
     thumbnail = models.ImageField()
+    thumbnail_file_thumb = ImageSpecField(
+        source="thumbnail",
+        processors=[ResizeToFill(800, 400)],
+        format="JPEG",
+        options={"quality": 60},
+    )
     # TODO = 업로드되는 파일이 비디오파일인지 여부를 검사하는 게 있으면 좋아
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
