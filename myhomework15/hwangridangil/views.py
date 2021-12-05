417da1c0904f7a967fa5f68803ab6bbfbbed7e1a
back hae ran <idg9833@gmail.com>
2021-12-05 21:19:35 +0900

    views(shop_detail,shop_list) 구현

--- myhomework15/hwangridangil/views.py
+++ myhomework15/hwangridangil/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpRequest,HttpResponse
+from hwangridangil.models import Shop
 
-# Create your views here.
+
+def shop_list(request: HttpRequest) -> HttpResponse:
+    qs = Shop.objects.all()
+    template_name = "hwangridangil/shop_list.html"
+    context_data = {"shop_list": qs}
+    return render(request,template_name,context_data)
+
+
+def shop_detail(request: HttpRequest) -> HttpResponse:
+    shop = Shop.objects.all()
+    template_name = "hwangridangil/shop_detail.html"
+    context_data = {"shop":shop}
+    return render(request,template_name,context_data)
