b924e4fc5150fc5713a319a6ad1d12dd29f0cb19
back hae ran <idg9833@gmail.com>
2021-12-05 15:40:31 +0900

    admin 화면 보여지기, 링크

--- myhome15/hwangridan/admin.py
+++ myhome15/hwangridan/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 from hwangridan.models import Shop
 
 
-admin.site.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ["id","name","description","latitude","longitude","telephone"]
+    list_display_links = ["name"]
+
+
+admin.site.register(Shop,ShopAdmin)
 
 
 
