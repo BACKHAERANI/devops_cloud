01c44b441d16cf5cd64f43ff1654ee3cd2be680e
back hae ran <idg9833@gmail.com>
2021-12-05 14:45:14 +0900

    myhome15 기본 설정 및 hawngridan 앱 만들고 모델 생성

--- myhome15/hwangridan/models.py
+++ myhome15/hwangridan/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Shop(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    description = models.TextField(blank=True)
+    address = models.CharField(max_length=100)
+    latitude = models.FloatField(verbose_name="위도")
+    longitude = models.FloatField(verbose_name="경도")
+    telephone = models.CharField(max_length=15)
+
