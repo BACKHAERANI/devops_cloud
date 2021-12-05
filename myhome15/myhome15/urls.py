9c5005a041906d428a7cd5f695d9225c0712cb70
back hae ran <idg9833@gmail.com>
2021-12-05 16:11:47 +0900

    레이아웃/리스트/리스트디테일/urls연결

--- myhome15/myhome15/urls.py
+++ myhome15/myhome15/urls.py
@@ -2,9 +2,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from hwangridan.views import shop_list, shop_detail
 
 urlpatterns = [
-    path('admin/', admin.site.urls),]
+    path('admin/', admin.site.urls),
+    path('hwangridan/', shop_list),
+    path('hwangridan/<int:pk>/', shop_detail),
+
+]
 
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
