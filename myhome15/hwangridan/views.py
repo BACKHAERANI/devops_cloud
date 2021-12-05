2a51829d3e5651b2055c95a2b80ce75c88189034
back hae ran <idg9833@gmail.com>
2021-12-05 15:59:04 +0900

    hwangridan views 구현

--- myhome15/hwangridan/views.py
+++ myhome15/hwangridan/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from hwangridan.models import Shop
 
-# Create your views here.
+
+def shop_list(request: HttpRequest) -> HttpResponse:
+    qs = Shop.objects.all()
+    template_name = "hwangridan/shop_list.html"
+    context_data ={"shop_list":qs, }
+    return render(request,template_name, context_data)
+
+
+def shop_detail(request: HttpRequest, pk:int) -> HttpResponse:
+    shop = Shop.objects.get(pk=pk)
+    template_name = "hwangridan/shop_detail"
+    context_data = {"shop": shop,}
+    return render(request, template_name,context_data)
