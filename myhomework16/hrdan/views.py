1eb493b3e91c60e057fda9eb2353b4be7d933ab5
back hae ran <idg9833@gmail.com>
2021-12-06 23:19:16 +0900

    views (shop_list 구현 )

--- myhomework16/hrdan/views.py
+++ myhomework16/hrdan/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from hrdan.models import Shop
 
-# Create your views here.
+
+def shop_list(request: HttpRequest) -> HttpResponse:
+    qs = Shop.objects.all()
+    template_name = "hrdan/shop_list.html"
+    context_data = {"shop_list": qs,}
+    return render(request, template_name, context_data)
+
