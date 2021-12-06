dafa44dc1f412f313e4a5cfa40e8e66907c46718
back hae ran <idg9833@gmail.com>
2021-12-06 16:39:41 +0900

    Post/Commend 구현

---
+++ mydjango16/blog/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from blog.models import Post, Comment
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "content", "created_at"]
+    list_display_links = ["title"]
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["pk", "post", "message", "created_at"]
+    list_display_links = ["post"]
+
+
