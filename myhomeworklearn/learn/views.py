ccad703692a4a4f1790d57ef42296a1b89d15bb6
back hae ran <idg9833@gmail.com>
2021-12-06 20:32:20 +0900

    views 구현 /layout/list/urls연결

--- myhomeworklearn/learn/views.py
+++ myhomeworklearn/learn/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from learn.models import Show
 
-# Create your views here.
+
+def show_list(request: HttpRequest) -> HttpResponse:
+    qs = Show.objects.all()
+    template_name = "learn/show_list.html"
+    context_data ={"show_list": qs,}
+    return render(request, template_name, context_data)
+
+
--- myhomeworklearn/myhomeworklearn/urls.py
+++ myhomeworklearn/myhomeworklearn/urls.py
@@ -2,9 +2,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from learn.views import show_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('learn/', show_list)
 ]
 
 urlpatterns += static(settings.MEDIA_URL,
