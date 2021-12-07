0c2c0fb2aec2bfbcf967f53e6c97bd7493d1084e
back hae ran <idg9833@gmail.com>
2021-12-08 00:19:56 +0900

    views 구현

--- myhomework17/mall/views.py
+++ myhomework17/mall/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from mall.models import Shop
 
-# Create your views here.
+
+def shop_list(request: HttpRequest) -> HttpResponse:
+    qs = Shop.objects.all()
+    return render(request, "mall/shop_list.html", {"shop_list": qs,})
+
+
+def shop_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    shop = Shop.objects.get(pk=pk)
+    return render(request, "mall/shop_detail.html", {"shop": shop,})
