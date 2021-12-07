41e0a12e4a1bece725029e0f2c4cd85f420b2da8
back hae ran <idg9833@gmail.com>
2021-12-07 16:21:11 +0900

    태크 링크 - 게시글 링크  구현

--- mydjango17/diary/urls.py
+++ mydjango17/diary/urls.py
@@ -4,5 +4,6 @@ from diary import views
 app_name = "diary"
 
 urlpatterns = [path("", views.post_list, name="post_list"),
-               path("<int:pk>/", views.post_detail, name="post_detail")
+               path("<int:pk>/", views.post_detail, name="post_detail"),
+               path("tags/<str:tag_name>/", views.tag_detail, name="tag_detail"),
                ]
