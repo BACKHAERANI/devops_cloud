9f4604a9933018329d41d8c12a37e3b6db41d0f9
back hae ran <idg9833@gmail.com>
2021-12-07 15:59:44 +0900

    게시판 구현 / 댓글, 태그 생성/settings 로깅설정

--- mydjango17/diary/views.py
+++ mydjango17/diary/views.py
@@ -5,11 +5,16 @@ from diary.models import Post
 
 def post_list(request: HttpRequest) -> HttpResponse:
     qs = Post.objects.all()
+    query = request.GET.get("query","")
+    if query:
+        qs = qs.filter(title__icontains=query)
     return render(request, "diary/post_list.html", {"post_list": qs})
 
 
 def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
     post = Post.objects.get(pk=pk)
+    comment_list = post.comment_set.all()
+    tag_list = post.tag_set.all()
     return render(request,"diary/post_detail.html",{
-        "post": post,
+        "post": post, "comment_list": comment_list, "tag_list" : tag_list,
     })
--- mydjango17/mydjango17/settings.py
+++ mydjango17/mydjango17/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'diary',
 ]
 
 if DEBUG:
@@ -134,5 +135,26 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-#django-debug-toolbar
+# django-debug-toolbar
+
 INTERNAL_IPS = ["127.0.0.1"]
+
+
+if DEBUG:
+    LOGGING = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "handlers": {
+            "console": {
+                "level": "DEBUG",
+                "class": "logging.StreamHandler",
+            },
+        },
+        "loggers": {
+            "django.db.backends": {
+                "handlers": ["console"],
+                "level": "DEBUG",
+                "propagate": False,
+            },
+        },
+    }
