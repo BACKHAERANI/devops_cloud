888ff2ebfaae181748ae1ad48b1a7b3336db7c56
back hae ran <idg9833@gmail.com>
2021-12-07 13:45:21 +0900

    detail 구현

---
+++ mydjango17/diary/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from diary.models import Post
+
+
+def post_list(request: HttpRequest) -> HttpResponse:
+    qs = Post.objects.all()
+    return render(request, "diary/post_list.html", {"post_list": qs})
+
+
+def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    post = Post.objects.get(pk=pk)
+    return render(request,"diary/post_detail.html",{
+        "post": post,
+    })
