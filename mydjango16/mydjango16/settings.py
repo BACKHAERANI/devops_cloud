4e5c78864b4f392f49473342673ce854fda271e0
back hae ran <idg9833@gmail.com>
2021-12-07 17:26:52 +0900

    mydjango16 누락파일

--- mydjango16/mydjango16/settings.py
+++ mydjango16/mydjango16/settings.py
@@ -31,14 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    #django apps
+    # django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #local apps
+    # local apps
     'blog',
 ]
 
--- mydjango16/mydjango16/urls.py
+++ mydjango16/mydjango16/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
 ]
-urlpatterns += [ static(settings.MEDIA_URL,
-                 document_root=settings.MEDIA_ROOT)]
+urlpatterns += static(settings.MEDIA_URL,
+                 document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
