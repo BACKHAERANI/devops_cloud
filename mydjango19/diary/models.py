ac159581fd3638cae4c772ee09ac8d6601ef13b9
back hae ran <idg9833@gmail.com>
2021-12-09 10:23:07 +0900

    1209 수업을 위한 project 생성

---
+++ mydjango19/diary/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+
+class TimestampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True    # 추상 클래스로서, DB테이블이 생기지 않음. 단순한 부모로서 등장
+
+
+class Post(TimestampedModel):
+    title = models.CharField(max_length=200, db_index=True)
+    content = models.TextField()
+    photo = models.ImageField(upload_to="diary/post/%Y/%m/%d")
+    tag_set = models.ManyToManyField("Tag", blank=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        verbose_name="포스팅"   # 단수형
+        verbose_name_plural="포스팅 목록"  # 복수형형
+
+
+class Comment(TimestampedModel):
+    # 외래키 : 정수값
+    post = models.ForeignKey(Post, on_delete= models.CASCADE)
+    # post_id - primary key
+    author_name = models.CharField(max_length=20)
+    message = models.TextField()
+
+    class Meta:
+        verbose_name = "댓글"  # 단수형
+        verbose_name_plural = "댓글 목록"  # 복수형형
+
+
+class Tag(TimestampedModel):
+    name = models.CharField(max_length=200, db_index=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name="태그"   # 단수형
+        verbose_name_plural="태그 목록"  # 복수형형
