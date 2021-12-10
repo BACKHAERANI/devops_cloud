e6e7eedb077c2b8cdb5ffb596f9ee2c533e75453
back hae ran <idg9833@gmail.com>
2021-12-10 18:19:15 +0900

    debug media 설정 shop/urls 추가

---
+++ mydjango20/shop/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from shop import views
+
+app_name = 'shop'
+
+
+urlpatterns=[path('new/', views.shop_new, name="shop_new"),
+             path('<int:pk>/', views.shop_detail, name="shop_detail"),]
+
