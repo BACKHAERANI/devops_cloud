e857227dba96bbab0acc9ca9e338af6c4f249c9f
back hae ran <idg9833@gmail.com>
2021-12-12 15:26:56 +0900

    shop_detail (태그버튼 생성) shop내urls 연결

--- mydjango20/shop/urls.py
+++ mydjango20/shop/urls.py
@@ -4,7 +4,8 @@ from shop import views
 
 app_name = 'shop'
 
-
-urlpatterns=[path('new/', views.shop_new, name="shop_new"),
-             path('<int:pk>/', views.shop_detail, name="shop_detail"),]
-
+urlpatterns = [path('', views.shop_list, name="shop_list"),
+               path('new/', views.shop_new, name="shop_new"),
+               path('<int:pk>/', views.shop_detail, name="shop_detail"),
+               path("tags/<str:tag_name>/", views.tag_detail, name="tag_detail"),
+               ]
