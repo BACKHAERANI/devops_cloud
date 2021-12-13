357bf6f8bf54c0c2730d6212d6f47742aefc2fe0
back hae ran <idg9833@gmail.com>
2021-12-13 16:48:42 +0900

    form을 통해 tags 초기값을 생성자를 통해서 지정

--- mydjango20/shop/forms.py
+++ mydjango20/shop/forms.py
@@ -5,6 +5,21 @@ from shop.models import Shop, Review, Tag
 class ShopForm(forms.ModelForm):
     tags = forms.CharField()
 
+    #부모 클래스의 생성자에서 어떤 인자를 지원하는지 잘 모르지만
+    # 생성자 호출시에 받은 인자를 그대로 부모에게 전달함
+
+    def __init__(self,*args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        #초기값 기억하기
+        if self.instance.pk:  # 수정시
+            tag_qs = self. instance.tag_set.all()
+            tags = ", ".join([tag.name for tag in tag_qs])
+            self.fields["tags"].initial = tags
+        # else: #새롭게생성
+        #     pass    
+      
+
     def save(self):
         #부모의 save를 호출해야함
         saved_post = super().save()
