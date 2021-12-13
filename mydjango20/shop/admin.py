b7ba30d03ce71df74193282c96e482a14292a1d8
back hae ran <idg9833@gmail.com>
2021-12-13 16:52:03 +0900

    admin에 shop에 ShopForm 수정

--- mydjango20/shop/admin.py
+++ mydjango20/shop/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-
+from shop.forms import ShopForm
 from shop.models import Shop, Review, Tag, Category
 
 
@@ -10,7 +10,7 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Shop)
 class ShopAdmin(admin.ModelAdmin):
-    pass
+    form = ShopForm
 
 
 @admin.register(Review)
