1a32d9b6ed253637a28893cc9cb16daeb9fec0a2
back hae ran <idg9833@gmail.com>
2021-12-15 13:27:47 +0900

    blog/urls 설정

--- mydjango23/blog/urls.py
+++ mydjango23/blog/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 
+from blog import views
+
 app_name = 'blog'
 
-urlpatterns = []
+urlpatterns = [path('', views.post_list, name='post_list'),
+               path('<int:pk>/', views.post_detail, name='post_detail'),
+               path('<int:pk>/edit', views.post_edit, name ='post_edit'),
+               path('new/', views.post_new, name='post_new'),
+               path('delete/',views.post_delete, name='post_delete')]
