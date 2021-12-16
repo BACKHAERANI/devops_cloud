01c0b36027175aec772be3a3e2288a4e4eabc7d4
back hae ran <idg9833@gmail.com>
2021-12-16 21:23:20 +0900

    model, admin

--- myhomework24/shop/models.py
+++ myhomework24/shop/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
-# Create your models here.
+
+class TimeStampeModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Category(TimeStampeModel):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Shop(TimeStampeModel):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, unique=True)
+    address = models.CharField(max_length=200)
+    photo = models.ImageField()
+    description = models.TextField()
+    tag_set = models.ManyToManyField('Tag', blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Review(TimeStampeModel):
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20, unique=True)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(TimeStampeModel):
+    name = models.CharField(max_length=10, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
