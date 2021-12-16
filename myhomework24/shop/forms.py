b9e5dda420a7367e4aa3288961de06362358ebea
back hae ran <idg9833@gmail.com>
2021-12-16 21:42:32 +0900

    form 설정

---
+++ myhomework24/shop/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from shop.models import Review, Shop
+
+
+class ShopForm(forms.ModelForm):
+    class Meta:
+        model = Shop
+        fields = '__all__'
+
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = "__all__"
