f6f0f898a1551586da963b014231decfb7a26259
back hae ran <idg9833@gmail.com>
2021-12-16 23:52:30 +0900

    forms

--- myhomework24/shop/forms.py
+++ myhomework24/shop/forms.py
@@ -1,11 +1,35 @@
 from django import forms
-from shop.models import Review, Shop
+from shop.models import Review, Shop, Tag
 
 
 class ShopForm(forms.ModelForm):
+    tags = forms.CharField()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance.pk:
+            tag_qs = self.instance.tag_set.all()
+            tags = ", ".join([tag.name for tag in tag_qs])
+            self.fields["tags"].initial = tags
+
+    def _save_m2m(self):
+        saved_shop = super()._save_m2m()
+        tag_list = []
+        tags = self.cleaned_data.get("tags", "")
+        for word in tags.split(","):
+            tag_name = word.strip()
+            tag, __ = Tag.objects.get_or_create(name=tag_name)
+            tag_list.append(tag)
+
+        self.instance.tag_set.clear()
+        self.instance.tag_set.add(*tag_list)
+
+        return saved_shop
+
     class Meta:
         model = Shop
-        fields = '__all__'
+        fields = ["category","name","photo","description"]
+
 
 
 class ReviewForm(forms.ModelForm):
