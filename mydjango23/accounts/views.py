5f22fe1ce915a5421ad04c8b63c4dc7372b3a936
back hae ran <idg9833@gmail.com>
2021-12-16 13:54:38 +0900

    accounts 프로필 로그인 로그아웃 회원가입 구현

--- mydjango23/accounts/views.py
+++ mydjango23/accounts/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.http import HttpRequest, HttpResponse
+from PIL import Image
 
-# Create your views here.
+
+def profile_image(request:HttpRequest) -> HttpResponse:
+    canvas = Image.new("RGBA", (256, 256), (135,117,179,300))
+    response = HttpResponse(content_type="image/png")
+    canvas.save(response, "PNG")
+    return response
+
+
+def login(request):
+    pass
+
+#
+# signup = CreateView.as_view(
+#     form_class=UserCreationForm,
+#     success_url=reverse_lazy("accounts:login"),
+#     template_name="accounts/signup_form.html")
+
+
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect("accounts:login")
+    else:
+        form = UserCreationForm()
+    return render(request, "accounts/signup_form.html",{"form": form})
+
+
+def profile(request):
+    pass
+
+
+def logout(request):
+    pass
+
