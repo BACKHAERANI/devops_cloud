ddc086374157097407b83b64a30c627426bd6e69
back hae ran <idg9833@gmail.com>
2021-12-16 16:08:43 +0900

    login profile 구현

--- mydjango23/accounts/views.py
+++ mydjango23/accounts/views.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
@@ -13,8 +15,7 @@ def profile_image(request:HttpRequest) -> HttpResponse:
     return response
 
 
-def login(request):
-    pass
+login = LoginView.as_view(template_name="accounts/login_form.html")
 
 #
 # signup = CreateView.as_view(
@@ -34,10 +35,10 @@ def signup(request):
     return render(request, "accounts/signup_form.html",{"form": form})
 
 
+@login_required
 def profile(request):
-    pass
+    return render(request, "accounts/profile.html")
 
 
-def logout(request):
-    pass
+logout = LogoutView.as_view(next_page="accounts:login",)
 
