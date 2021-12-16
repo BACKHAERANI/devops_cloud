9c36eb38ab67ec4363e4d923da63f5c5c685c75c
back hae ran <idg9833@gmail.com>
2021-12-16 16:20:19 +0900

    프로젝트 레벨의 mydjango/templates/layout 구현

--- mydjango23/mydjango23/settings.py
+++ mydjango23/mydjango23/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'mydjango23.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/ "mydjango23/templates",],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
