cf84e1f6c012fabff7d92123d386c02bd456855e
back hae ran <idg9833@gmail.com>
2021-12-17 14:32:06 +0900

    shop_list  구현

--- mydjango25/shop/urls.py
+++ mydjango25/shop/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 
+from shop import views
+
 app_name='shop'
 
-urlpatterns = []     #urlpatterns는 빈 리스트라도 무조건 있어야 한다.
+urlpatterns = [path('', views.shop_list, name='shop_list'),
+               path('<int:pk>/', views.shop_detail, name='shop_detail'),
+
+
+               ]     #urlpatterns는 빈 리스트라도 무조건 있어야 한다.
