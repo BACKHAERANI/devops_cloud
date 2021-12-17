2e7bcf693eb1994b0e6708d6c84f2396c0e6bef9
back hae ran <idg9833@gmail.com>
2021-12-17 16:40:12 +0900

    review edit 구현

--- mydjango25/shop/urls.py
+++ mydjango25/shop/urls.py
@@ -7,6 +7,7 @@ app_name='shop'
 urlpatterns = [path('', views.shop_list, name='shop_list'),
                path('<int:pk>/', views.shop_detail, name='shop_detail'),
                path('<int:shop_pk>/reviews/new/', views.review_new, name = "review_new"),
+               path('<int:shop_pk>/reviews/<int:pk>/edit/', views.review_edit, name="review_edit"),
 
 
                ]     #urlpatterns는 빈 리스트라도 무조건 있어야 한다.
--- mydjango25/shop/views.py
+++ mydjango25/shop/views.py
@@ -1,6 +1,10 @@
-from django.views.generic import ListView, DetailView
-
-from shop.models import Shop, Category
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.shortcuts import redirect, get_object_or_404, resolve_url
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from shop.forms import ReviewForm
+from shop.mixins import ReviewUserCheckMixin
+from shop.models import Shop, Category, Review
 
 
 class ShopListView(ListView):
@@ -16,6 +20,39 @@ shop_list = ShopListView.as_view(model=Shop,)
 
 shop_detail = DetailView.as_view(model=Shop,)
 
+
+class ReviewCreateView(LoginRequiredMixin, CreateView):     #LoginRequiredMixin: 로그인해야 사용할 수 있게 함
+    model = Review
+    form_class = ReviewForm
+    # success_url = reverse_lazy("shop:shop_list")
+
+    def form_valid(self, form):
+        # self.kwargs :url Captured 값이 사전으로 저장
+        shop_pk = self.kwargs["shop_pk"]
+        shop = get_object_or_404(Shop, pk=shop_pk)
+
+        review = form.save(commit=False)
+        review.shop = shop
+        review.user = self.request.user
+        review.save()
+        # return redirect("shop:shop_detail")
+        return redirect(shop)
+
+
+review_new = ReviewCreateView.as_view()
+
+
+class ReviewUpdateView(LoginRequiredMixin, ReviewUserCheckMixin, UpdateView):
+    model = Review
+    form_class = ReviewForm
+    # success_url = reverse_lazy("shop:shop_list")
+
+    def get_success_url(self) -> str:
+        review = self.object
+        return resolve_url(review.shop)
+
+
+review_edit = ReviewUpdateView.as_view()
 
 
 
