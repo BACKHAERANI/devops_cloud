5e4862cfc7b175646299aef7eb575f309135c54c
back hae ran <idg9833@gmail.com>
2021-12-17 11:47:40 +0900

    settings에 앱추가

--- mydjango25/mydjango25/settings.py
+++ mydjango25/mydjango25/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap5',
+    'shop',
+    'accounts',
 
 ]
 
@@ -114,7 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
 TIME_ZONE = 'UTC'
 
