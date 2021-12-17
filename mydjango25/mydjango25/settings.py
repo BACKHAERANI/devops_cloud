a0de2611f8de8d08fad1a366dbc7ae66c81663f2
back hae ran <idg9833@gmail.com>
2021-12-17 10:34:22 +0900

    debug 설정, bootstrap5 설정

--- mydjango25/mydjango25/settings.py
+++ mydjango25/mydjango25/settings.py
@@ -37,8 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap5',
+
 ]
 
+if DEBUG:
+    INSTALLED_APPS += ['debug_toolbar']
 
 
 MIDDLEWARE = [
@@ -50,6 +54,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+if DEBUG:
+    MIDDLEWARE = ['debug_toolbar.middleware.DebugToolbarMiddleware'] + MIDDLEWARE
 
 ROOT_URLCONF = 'mydjango25.urls'
 
@@ -128,3 +135,5 @@ MEDIA_ROOT = BASE_DIR/'media'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = ["127.0.0.1"]
