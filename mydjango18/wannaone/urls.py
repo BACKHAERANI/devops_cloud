3a1165241fb87c5297610aadfbf9ae669798d78f
back hae ran <idg9833@gmail.com>
2021-12-18 17:37:02 +0900

    mydjango18 urls views html

--- mydjango18/wannaone/urls.py
+++ mydjango18/wannaone/urls.py
@@ -5,4 +5,5 @@ app_name = "wannaone"
 
 urlpatterns = [path("", views.star_list, name="star_list"),
                path("<int:pk>", views.star_detail, name="star_detail"),
+               path("tags/<str:tag_name>/", views.tag_detail, name="tag_detail")
                ]
--- mydjango18/wannaone/views.py
+++ mydjango18/wannaone/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
+from wannaone.form import StarForm
 from wannaone.models import Star
 
 
-def tag_detail(request: HttpRequest, tag_name:str) -> HttpResponse:
+def tag_detail(request: HttpRequest, tag_name: str) -> HttpResponse:
     qs = Star.objects.all()
     qs = qs.filter(tag_set__name=tag_name)
-    return render(request, "wannaone/tag_detail.html", {"tag_detail":qs,})
+    return render(request, "wannaone/tag_detail.html", {"tag_name":tag_name, "star_list":qs})
 
 
 def star_list(request: HttpRequest) -> HttpResponse:
@@ -19,4 +21,20 @@ def star_list(request: HttpRequest) -> HttpResponse:
 
 def star_detail(request: HttpRequest, pk: int) -> HttpResponse:
     star = Star.objects.get(pk=pk)
-    return render(request, "wannaone/star_detail.html", {"star": star})
+    tag_list = star.tag_set.all()
+    return render(request, "wannaone/star_detail.html", {"star": star,  "tag_list" : tag_list})
+
+def star_new(request: HttpRequest) ->HttpResponse:
+    if request.method = "POST":
+        form = StarForm(request.POST, request.FILES)
+        if form.is_vaild():
+            star = form.save(commit=False)
+            star.ip = request.META("REMOTE_ADDR")
+            form.save()
+            messages.success(request, "성공적으로 등록되었습니다.")
+            return redirect("wannaone:star_list")
+        else:
+            form = StarForm
+
+        return render(request, "wannaone/star_form.html",{"form":form})
+
