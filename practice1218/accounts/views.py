71c3132d5dc6f6cc1b63123c4a2e0389cd2dcab7
back hae ran <idg9833@gmail.com>
2021-12-18 19:26:05 +0900

    accounts views 생성 urls 연결

--- practice1218/accounts/views.py
+++ practice1218/accounts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import CreateView, TemplateView
 
-# Create your views here.
+signup = CreateView.as_view(form_class=UserCreationForm, template_name='accounts/sign.html')
+
+login = LoginView.as_view(template_name='accounts/login.html')
+
+logout = LogoutView.as_view(next_page='practice1218/root.html')
+
+profile = login_required(TemplateView.as_view(template_name='accounts/profile.html'))
+
