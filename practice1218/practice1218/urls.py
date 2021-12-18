419da37196bcada7fe43a56f74d4b0ef71004209
back hae ran <idg9833@gmail.com>
2021-12-18 18:39:34 +0900

    accounts 앱 생성 하고 settings 등록/ urls 생성.등록

--- practice1218/practice1218/urls.py
+++ practice1218/practice1218/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('bake/', include('bake.urls')),
+    path('accounts/', include('accounts.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL,
