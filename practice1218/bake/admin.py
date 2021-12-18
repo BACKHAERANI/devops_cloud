4e6ef9e18be62019410036ef05e1fb45a03a11d3
back hae ran <idg9833@gmail.com>
2021-12-18 18:27:05 +0900

    bake models, admin 생성하고 urls 등록

--- practice1218/bake/admin.py
+++ practice1218/bake/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from bake.models import Bake, Comment, Tag, Category
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Bake)
+class BakeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
--- practice1218/bake/models.py
+++ practice1218/bake/models.py
@@ -1,3 +1,58 @@
 from django.db import models
 
-# Create your models here.
+
+class Timestamp(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Category(Timestamp):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Bake(Timestamp):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=300, db_index=True)
+    content = models.TextField()
+    photo = models.ImageField(blank=True)
+    tag_set = models.ManyToManyField('Tag', blank=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ["-id"]
+
+
+class Comment(Timestamp):
+    bake = models.ForeignKey(Bake, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.message
+
+    class Meta:
+        ordering = ["-id"]
+
+
+class Tag(Timestamp):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+
+
+
