3ccc7b0048d3348f44004df2cb33506836d6040c
back hae ran <idg9833@gmail.com>
2021-12-23 23:00:19 +0900

    List 베이직리스트 적용

--- myreact29/src/MelonSearch.jsx
+++ myreact29/src/MelonSearch.jsx
@@ -1,8 +1,8 @@
 import { Input } from 'antd';
 import { useState } from 'react';
-
 import Axios from 'axios';
 import jsonpAdapter from 'axios-jsonp';
+import { List, Avatar } from 'antd';
 
 function MelonSearch() {
   const [query, setQuery] = useState('');
@@ -64,12 +64,21 @@ function MelonSearch() {
       />
       {songList.map((song) => {
         return (
-          <div>
-            <img src={song.ALBUMIMG} />
-            {song.SONGNAME} by {song.ARTISTNAME}
-          </div>
+          <List
+    itemLayout="horizontal"
+    dataSource={songList}
+    renderItem={(song) => (
+      <List.Item>
+        <List.Item.Meta
+          avatar={<Avatar src={song.ALBUMIMG} />}
+          title={song.SONGNAME}
+          description={song.ARTISTNAME}
+        />
+      </List.Item>
+        )}
+        />
         );
-      })}
+    })}
     </div>
   );
 }
