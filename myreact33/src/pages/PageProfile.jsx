1303eb09f4d057ca9ca126f507abe34f7f15075e
back hae ran <idg9833@gmail.com>
2021-12-30 13:45:16 +0900

    clear 버튼 생성

--- myreact33/src/pages/PageProfile.jsx
+++ myreact33/src/pages/PageProfile.jsx
@@ -1,21 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Axios from "axios";
 
 function PageProfile() {
-  const [profileList] = useState([
-    // {
-    //   unique: "bts-jin",
-    //   name: "진",
-    //   role: "서브보컬",
-    //   mbti: "INFP",
-    //   instagramurl: "https://instagram.com/jin",
-    //   profileimageurl:
-    //     "https://classdevopscloud.blob.core.windows.net/data/bts-jin.jpg",
-    // },
-  ]);
+  const [profileList, setProfileList] = useState([]);
+
+  useEffect(() => {
+    Axios.get(
+      "https://classdevopscloud.blob.core.windows.net/data/profile-list.json"
+    )
+      .then((response) => {
+        setProfileList(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
 
   return (
     <div>
       <h2>PageProfile</h2>
+      <button onClick={() => setProfileList([])}>clear</button>
 
       {profileList.length == 0 && <h3>등록된 프로필이 없습니다.</h3>}
 
@@ -23,12 +27,12 @@ function PageProfile() {
         return (
           <>
             <h3>
-              [{member.unique}]{member.name}
+              [{member.unique_id}]{member.name}
             </h3>
             <ul>
-              <img src={member.profileimageurl} />
+              <img src={member.profile_image_url} />
               <li>{member.role}</li>
-              <li>{member.instagramurl}</li>
+              <li>{member.instagram_url}</li>
               <li>{member.mbti}</li>
             </ul>
           </>
