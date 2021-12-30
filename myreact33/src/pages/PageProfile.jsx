b91f40adb7fc61cecef0fca7603e3b5db5d18e41
back hae ran <idg9833@gmail.com>
2021-12-30 17:57:20 +0900

    검색기능 구현

--- myreact33/src/pages/PageProfile.jsx
+++ myreact33/src/pages/PageProfile.jsx
@@ -4,6 +4,7 @@ import Axios from "axios";
 function PageProfile() {
   const [profileList, setProfileList] = useState([]);
   const [checkError, setCheckError] = useState(null);
+  const [query, setQuery] = useState("");
 
   const handleRefresh = () => {
     setCheckError(null);
@@ -31,6 +32,19 @@ function PageProfile() {
     handleRefresh();
   }, []);
 
+  const handleChange = (e) => {
+    const value = e.target.value;
+    console.log(value);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      console.log("ENTER");
+      const value = e.target.value;
+      setQuery(value);
+    }
+  };
+
   return (
     <div>
       <h2>PageProfile</h2>
@@ -39,27 +53,41 @@ function PageProfile() {
 
       <button onClick={handleRefresh}>새로고침</button>
 
+      <input
+        type="text"
+        placeholder="검색어를 입력해주세요."
+        onChange={handleChange}
+        onKeyPress={handleKeyPress}
+      />
       {profileList.length == 0 && !checkError && (
         <h3>등록된 프로필이 없습니다.</h3>
       )}
 
       {checkError && <h3>오류가 발생했습니다. 잠시후 다시 시도해주세요.</h3>}
 
-      {profileList.map((member) => {
-        return (
-          <>
-            <h3>
-              [{member.unique}]{member.name}
-            </h3>
-            <ul>
-              <img src={member.profileimageurl} />
-              <li>{member.role}</li>
-              <li>{member.instagramurl}</li>
-              <li>{member.mbti}</li>
-            </ul>
-          </>
-        );
-      })}
+      {profileList
+        .filter((member) => {
+          return (
+            member.name.includes(query) ||
+            member.role.includes(query) ||
+            member.mbti.includes(query)
+          );
+        })
+        .map((member) => {
+          return (
+            <>
+              <h3>
+                [{member.unique}]{member.name}
+              </h3>
+              <ul>
+                <img src={member.profileimageurl} />
+                <li>{member.role}</li>
+                <li>{member.instagramurl}</li>
+                <li>{member.mbti}</li>
+              </ul>
+            </>
+          );
+        })}
     </div>
   );
 }
