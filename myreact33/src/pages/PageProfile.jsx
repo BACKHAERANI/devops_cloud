04182c2f49e08b86f550f941be41bd3d81f243d0
back hae ran <idg9833@gmail.com>
2021-12-30 11:37:41 +0900

    초기 상태값 빈어레이로 넣고 문자열길이가 0이면 등록프로필이 없다고 리턴

--- myreact33/src/pages/PageProfile.jsx
+++ myreact33/src/pages/PageProfile.jsx
@@ -2,20 +2,22 @@ import { useState } from "react";
 
 function PageProfile() {
   const [profileList] = useState([
-    {
-      unique: "bts-jin",
-      name: "진",
-      role: "서브보컬",
-      mbti: "INFP",
-      instagramurl: "https://instagram.com/jin",
-      profileimageurl:
-        "https://classdevopscloud.blob.core.windows.net/data/bts-jin.jpg",
-    },
+    // {
+    //   unique: "bts-jin",
+    //   name: "진",
+    //   role: "서브보컬",
+    //   mbti: "INFP",
+    //   instagramurl: "https://instagram.com/jin",
+    //   profileimageurl:
+    //     "https://classdevopscloud.blob.core.windows.net/data/bts-jin.jpg",
+    // },
   ]);
 
   return (
     <div>
       <h2>PageProfile</h2>
+
+      {profileList.length == 0 && <h3>등록된 프로필이 없습니다.</h3>}
 
       {profileList.map((member) => {
         return (
