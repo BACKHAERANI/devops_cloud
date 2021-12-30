cf831409908d761ace9ca76765435b8b7599a62b
back hae ran <idg9833@gmail.com>
2021-12-30 15:36:57 +0900

    error 상태값 추가로 정의하고 에러메세지 노출하기

--- myreact33/src/pages/PageProfile.jsx
+++ myreact33/src/pages/PageProfile.jsx
@@ -3,7 +3,10 @@ import Axios from "axios";
 
 function PageProfile() {
   const [profileList, setProfileList] = useState([]);
+  const [checkError, setCheckError] = useState(null);
+
   const handleRefresh = () => {
+    setCheckError(null);
     Axios.get(
       "https://classdevopscloud.blob.core.windows.net/data/profile-list.json"
     )
@@ -17,35 +20,25 @@ function PageProfile() {
         setProfileList(profileList);
       })
       .catch((error) => {
-        console.error(error);
+        setCheckError(error);
       });
   };
 
   useEffect(() => {
-    Axios.get(
-      "https://classdevopscloud.blob.core.windows.net/data/profile-list.json"
-    )
-      .then((response) => {
-        const profileList = response.data.map((profile) => ({
-          ...profile,
-          unique: profile.unique_id,
-          profileimageurl: profile.profile_image_url,
-          instagramurl: profile.instagram_url,
-        }));
-        setProfileList(profileList);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    handleRefresh();
   }, []);
 
   return (
     <div>
       <h2>PageProfile</h2>
+
       <button onClick={() => setProfileList([])}>clear</button>
+
       <button onClick={handleRefresh}>새로고침</button>
 
       {profileList.length == 0 && <h3>등록된 프로필이 없습니다.</h3>}
+
+      {checkError && <h3>오류가 발생했습니다. 잠시후 다시 시도해주세요.</h3>}
 
       {profileList.map((member) => {
         return (
