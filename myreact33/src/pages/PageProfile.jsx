f27c344ab9926c89994928cc84938fd74577a21a
back hae ran <idg9833@gmail.com>
2021-12-30 11:36:38 +0900

    상태값 map사용하여 리턴하기

--- myreact33/src/pages/PageProfile.jsx
+++ myreact33/src/pages/PageProfile.jsx
@@ -1,8 +1,38 @@
+import { useState } from "react";
+
 function PageProfile() {
+  const [profileList] = useState([
+    {
+      unique: "bts-jin",
+      name: "진",
+      role: "서브보컬",
+      mbti: "INFP",
+      instagramurl: "https://instagram.com/jin",
+      profileimageurl:
+        "https://classdevopscloud.blob.core.windows.net/data/bts-jin.jpg",
+    },
+  ]);
+
   return (
-    <>
+    <div>
       <h2>PageProfile</h2>
-    </>
+
+      {profileList.map((member) => {
+        return (
+          <>
+            <h3>
+              [{member.unique}]{member.name}
+            </h3>
+            <ul>
+              <img src={member.profileimageurl} />
+              <li>{member.role}</li>
+              <li>{member.instagramurl}</li>
+              <li>{member.mbti}</li>
+            </ul>
+          </>
+        );
+      })}
+    </div>
   );
 }
 
