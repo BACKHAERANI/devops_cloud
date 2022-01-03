d174cb6466ce7e5997281051182fdef610c9eafe
back hae ran <idg9833@gmail.com>
2022-01-03 18:51:20 +0900

    초기값, 초기컬러 map 사용해서 적용하기

--- myhomework35/src/SevelNumbers.jsx
+++ myhomework35/src/SevelNumbers.jsx
@@ -1,11 +1,47 @@
 import { useState } from "react";
 
 function SevelNumbers() {
+  // const [state, setState] = useState();
+  // const { numbers, colors } = state;
+  const [state, setState] = useState({
+    numbers: [0, 0, 0, 0, 0, 0, 0],
+    colors: [
+      "#1B62BF",
+      "#1755A6",
+      "#37A647",
+      "#F29F05",
+      "#F23838",
+      "purple",
+      "pink",
+    ],
+  });
+  const { numbers, colors } = state;
+
   return (
     <div>
       <h2>7개의 숫자</h2>
+      {numbers.map((number, index) => {
+        return (
+          <div
+            style={{ ...defaultStyle, backgroundColor: state.colors[index] }}
+          >
+            {number}
+          </div>
+        );
+      })}
     </div>
   );
 }
+
+const defaultStyle = {
+  width: "100px",
+  height: "100px",
+  borderRadius: "50px",
+  lineHeight: "100px",
+  textAlign: "center",
+  display: "inline-block",
+  fontSize: "3rem",
+  userSelect: "none",
+};
 
 export default SevelNumbers;
