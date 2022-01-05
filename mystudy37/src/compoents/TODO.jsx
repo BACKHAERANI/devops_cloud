b7edcee12021220cd4cbf5e1519b6cf564c16cf6
back hae ran <idg9833@gmail.com>
2022-01-05 16:14:20 +0900

    removeTodo 구현 (클릭하면 사라지도록)

--- mystudy37/src/compoents/TODO.jsx
+++ mystudy37/src/compoents/TODO.jsx
@@ -5,11 +5,17 @@ const INITIAL_STATE = [{ content: '파이썬' }, { content: '자바스크립트'
 function TODO() {
   const [todoList, setTodoList] = useState(INITIAL_STATE);
 
+  const removeTodo = (todoIndex) => {
+    setTodoList((prevTodoList) =>
+      prevTodoList.filter((_, index) => index !== todoIndex),
+    );
+  };
+
   return (
     <div>
       <h1>TODOLIST</h1>
       {todoList.map((todo, index) => (
-        <div>{todo.content}</div>
+        <div onClick={() => removeTodo(index)}>{todo.content}</div>
       ))}
     </div>
   );
