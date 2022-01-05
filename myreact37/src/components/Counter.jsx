e75fdec6ed72721d90fc8f2a808a1bfab450e14e
back hae ran <idg9833@gmail.com>
2022-01-05 11:10:54 +0900

    Counter useReducer 구현

--- myreact37/src/components/Counter.jsx
+++ myreact37/src/components/Counter.jsx
@@ -1,23 +1,40 @@
 import { useReducer, useState } from 'react';
-import 'components/Counter.css';
+import './Counter.css';
+
+const ACTION_TYPES = {
+  INCREASE: 'INCREASE',
+  DECREASE: 'DECREASE',
+};
+
+function reducer(prevState, action) {
+  const { type } = action;
+  if (type === ACTION_TYPES.INCREASE) {
+    return prevState + 1;
+  }
+  return prevState; // 방어
+}
 
 function Counter() {
-  const [value, setValue] = useState(0);
+  // const [value, setValue] = useState(0);
+  const [value, dispatch] = useReducer(reducer, 0);
 
-  const handleUp = () => {
-    setValue(value + 1);
+  const handleClick = () => {
+    // setValue((prevValue) => prevValue + 1);
+    dispatch({ type: ACTION_TYPES.INCREASE });
   };
 
-  const handleDown = () => {
-    setValue(value - 1);
+  const handleRightClick = (e) => {
+    e.preventDefault();
+    // setValue((prevValue) => prevValue - 1);
+    dispatch({ type: ACTION_TYPES.DECREASE });
   };
 
   return (
     <div
       className="counter"
       style={{ backgroundColor: 'red' }}
-      onClick={handleUp}
-      onContextMenu={handleDown}
+      onClick={handleClick}
+      onContextMenu={handleRightClick}
     >
       {value}
     </div>
