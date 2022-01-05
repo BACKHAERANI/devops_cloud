b2a5d9558059b20724930ddc45457330faf17676
back hae ran <idg9833@gmail.com>
2022-01-05 18:34:05 +0900

    css 적용/ Review.jsx 생성

---
+++ myhomework37/src/compoents/ReviewList.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import View from 'compoents/Review';
+import './Review-list.css';
+
+const Basic_Review = [
+  { content: '스파이더맨...보고 싶었는데 볼 시간이 없었어요...' },
+  { content: 'MY...MJ...' },
+];
+
+function ReviewList() {
+  const [reviewList, setReviewList] = useState(Basic_Review);
+  return (
+    <div className="Review-list">
+      <h1>ReviewList</h1>
+      {reviewList.map((review) => (
+        <View review={review}>{review.content}</View>
+      ))}
+    </div>
+  );
+}
+
+export default ReviewList;
