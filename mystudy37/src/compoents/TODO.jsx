c4b879158739f7a53399a7ef3617cff640d54578
back hae ran <idg9833@gmail.com>
2022-01-05 16:10:17 +0900

    todoList 리턴 구현 (map)

--- mystudy37/src/compoents/TODO.jsx
+++ mystudy37/src/compoents/TODO.jsx
@@ -1,7 +1,16 @@
+import { useState } from 'react';
+
+const INITIAL_STATE = [{ content: '파이썬' }, { content: '자바스크립트' }];
+
 function TODO() {
+  const [todoList, setTodoList] = useState(INITIAL_STATE);
+
   return (
     <div>
       <h1>TODOLIST</h1>
+      {todoList.map((todo, index) => (
+        <div>{todo.content}</div>
+      ))}
     </div>
   );
 }
