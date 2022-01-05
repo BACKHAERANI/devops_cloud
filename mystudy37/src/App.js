1d1da2bb4482a355d45f9f38e60f7030753e6e30
back hae ran <idg9833@gmail.com>
2022-01-05 16:02:24 +0900

    TODO 최소한의 구현

--- mystudy37/src/App.js
+++ mystudy37/src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import PageTODO from './Pages/PageTODO';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <PageTODO />
+    </div>
+  );
 }
 
 export default App;
---
+++ mystudy37/src/Pages/PageTODO.jsx
@@ -0,0 +1,11 @@
+import TODO from '../compoents/TODO';
+
+function PageTODO() {
+  return (
+    <div>
+      <TODO />
+    </div>
+  );
+}
+
+export default PageTODO;
