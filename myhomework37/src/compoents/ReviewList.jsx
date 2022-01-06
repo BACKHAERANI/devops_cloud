39d24fceda59a91d66f7b3ed54e3bfd744358249
back hae ran <idg9833@gmail.com>
2022-01-06 13:17:54 +0900

    id 추가

--- myhomework37/src/compoents/ReviewList.jsx
+++ myhomework37/src/compoents/ReviewList.jsx
@@ -4,20 +4,27 @@ import './Review-list.css';
 import usefieldValues from './hooks/useFieldValues';
 import ReviewForm from './ReviewForm';
 
-const Basic_Review = [
-  { content: '스파이더맨...보고 싶었는데 볼 시간이 없었어요...', score: 3 },
-  { content: 'MY...MJ...', score: 4 },
+const INITIAL_STATE = [
+  {
+    id: 1,
+    content: '스파이더맨...보고 싶었는데 볼 시간이 없었어요...',
+    score: 3,
+  },
+  { id: 2, content: 'MY...MJ...', score: 4 },
+  { id: 3, content: '스파이더맨...도 좋지만...토르', score: 4 },
 ];
 
 function ReviewList() {
-  const [reviewList, setReviewList] = useState(Basic_Review);
+  const [reviewList, setReviewList] = useState(INITIAL_STATE);
   const [form, setForm] = useState(false);
   const [fieldValues, handleChange, clearFieldValues] = usefieldValues({
     content: '',
+    score: 5,
   });
 
   const appendReview = () => {
-    const review = { ...fieldValues };
+    const reviewId = new Date().getTime();
+    const review = { ...fieldValues, Id: reviewId };
     setReviewList((prevReviewList) => [...prevReviewList, review]);
     clearFieldValues();
   };
