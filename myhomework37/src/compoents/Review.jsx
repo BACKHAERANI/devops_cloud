ec432389119bc833ebbb8c4c6f7f2c6738e7c87c
back hae ran <idg9833@gmail.com>
2022-01-06 13:44:50 +0900

    수정/ 삭제 핸들러 구현

--- myhomework37/src/compoents/Review.jsx
+++ myhomework37/src/compoents/Review.jsx
@@ -1,8 +1,24 @@
 import Star from 'compoents/Star';
 import { useState } from 'react';
 
-function Review({ review }) {
+function Review({ review, handleEdit, handleDelete }) {
   const [showMenu, setShowMenu] = useState(false);
+
+  const handleClickedDeletdButton = () => {
+    if (handleDelete) {
+      handleDelete();
+    } else {
+      console.warn('[Review]handleDelete 속성값이 지정되지 않았습니다.');
+    }
+  };
+
+  const handleClickedEditdButton = () => {
+    if (handleEdit) {
+      handleEdit();
+    } else {
+      console.warn('[Review]handleEdit 속성값이 지정되지 않았습니다.');
+    }
+  };
 
   return (
     <div
@@ -14,8 +30,18 @@ function Review({ review }) {
     >
       {showMenu && (
         <div className="text-xs absolute top-0 right-0 hover:cursor-pointer ">
-          <span className="mr-1 text-blue-700 hover:bg-blue-400">수정</span>
-          <span className="text-red-500 hover:bg-red-300">삭제</span>
+          <span
+            onClick={() => handleClickedEditdButton()}
+            className="mr-1 text-blue-700 hover:bg-blue-400"
+          >
+            수정
+          </span>
+          <span
+            onClick={() => handleClickedDeletdButton()}
+            className="text-red-500 hover:bg-red-300"
+          >
+            삭제
+          </span>
         </div>
       )}
 
