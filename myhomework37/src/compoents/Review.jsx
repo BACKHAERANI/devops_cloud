be5f91b566c2dfa6d8cd6c7b239443913d0fc0a1
back hae ran <idg9833@gmail.com>
2022-01-06 13:33:35 +0900

    수정/ 삭제 버튼 형태 스타일 구현

--- myhomework37/src/compoents/Review.jsx
+++ myhomework37/src/compoents/Review.jsx
@@ -1,13 +1,24 @@
 import Star from 'compoents/Star';
+import { useState } from 'react';
 
 function Review({ review }) {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
     <div
-      className="bg-white-200 
-    hover:bg-green-400 m-1 p-1  border-green-200 border-2 
-    hover:border-green-300 
-    hover:cursor-pointer"
+      onMouseEnter={() => setShowMenu(true)}
+      onMouseLeave={() => setShowMenu(false)}
+      className="bg-white 
+    hover:bg-green-200 m-1 p-1 pt-4  border-green-200 border-2 
+    hover:border-green-500 relative "
     >
+      {showMenu && (
+        <div className="text-xs absolute top-0 right-0 hover:cursor-pointer ">
+          <span className="mr-1 text-blue-700 hover:bg-blue-400">수정</span>
+          <span className="text-red-500 hover:bg-red-300">삭제</span>
+        </div>
+      )}
+
       {review.content}
       <Star score={review.score} />
     </div>
